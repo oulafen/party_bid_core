bebdfe86a35cfbdb3418d75f31343b1b0b6f424a
xiaofen <oulafen@gmail.com>
2014-02-10 13:51:06 +0800

    完成结构一中的bid_apec.js

--- data_structure_1/src/Activity.js
+++ data_structure_1/src/Activity.js
@@ -7,10 +7,20 @@ function Activity(name, sign_ups, bids) {
     this.bids = bids;
 }
 Activity.save_activity_to_activities = function (activity) {
-    var activities = JSON.parse(localStorage.getItem('activities')) ;
+    var activities = JSON.parse(localStorage.getItem('activities'));
     activities.unshift(activity);
     localStorage.setItem('activities', JSON.stringify(activities));
 }
-Activity.save_activity_name_to_current_activity = function(activity_name){
-    localStorage.setItem('current_activity',activity_name);
+Activity.save_activity_name_to_current_activity = function (activity_name) {
+    localStorage.setItem('current_activity', activity_name);
+}
+Activity.get_current_activity = function () {
+    var activities = JSON.parse(localStorage.getItem('activities'));
+    var name = localStorage.getItem('current_activity');
+    return _.find(activities, function (activity) {
+        return activity.name == name;
+    });
+}
+Activity.get_activities = function () {
+    return JSON.parse(localStorage.activities);
 }
--- data_structure_1/src/Bid.js
+++ data_structure_1/src/Bid.js
@@ -1,3 +1,23 @@
 /**
  * Created by xiaofen on 14-2-10.
  */
+function Bid(name, biddings) {
+    this.name = name;
+    this.biddings = biddings;
+}
+Bid.create_new_bid = function (active_name) {
+    var bid_name = Bid.get_bid_name();
+    var bid = new Bid(bid_name, []);
+    var bids = Activity.get_current_activity().bids;
+    bids.push(bid);
+    var activities=Activity.get_activities();
+    _.map(activities, function (activity) {
+        if (activity.name == active_name) {
+            activity.bids=bids;
+        }
+    });
+    localStorage.activities=JSON.stringify(activities);
+}
+Bid.get_bid_name = function () {
+    return ('竞价' + (Activity.get_current_activity().bids.length + 1));
+}
