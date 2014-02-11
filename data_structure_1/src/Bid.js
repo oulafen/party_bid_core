63dc590fdf510bfd045d67c1fcf288a72886c326
xiaofen <oulafen@gmail.com>
2014-02-11 15:19:38 +0800

    can show all bids

--- data_structure_1/src/Bid.js
+++ data_structure_1/src/Bid.js
@@ -10,14 +10,21 @@ Bid.create_new_bid = function (active_name) {
     var bid = new Bid(bid_name, []);
     var bids = Activity.get_current_activity().bids;
     bids.push(bid);
-    var activities=Activity.get_activities();
+    var activities = Activity.get_activities();
     _.map(activities, function (activity) {
         if (activity.name == active_name) {
-            activity.bids=bids;
+            activity.bids = bids;
         }
     });
-    localStorage.activities=JSON.stringify(activities);
+    localStorage.activities = JSON.stringify(activities);
 }
 Bid.get_bid_name = function () {
     return ('竞价' + (Activity.get_current_activity().bids.length + 1));
+}
+Bid.get_bids = function (activity_name) {
+    var activity = Activity.get_activity_by_activity_name(activity_name);
+    return activity.bids;
+}
+function transform_bids_to_view_model(activity_name) {
+    return Bid.get_bids(activity_name);
 }
