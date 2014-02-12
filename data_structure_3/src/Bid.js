49ff1b345090765b357c627aefe56cfaebc104b1
xiaofen <oulafen@gmail.com>
2014-02-12 15:15:08 +0800

    data_structure_3-->完成create_new_bid的功能

--- data_structure_3/src/Bid.js
+++ data_structure_3/src/Bid.js
@@ -1,3 +1,12 @@
-/**
- * Created by xiaofen on 14-2-12.
- */
+function Bid(activity_id) {
+    this.name = '';
+    this.activity_id = activity_id;
+    this.biddings = []
+}
+Bid.create_new_bid = function (activity_id) {
+    var bid = new Bid(activity_id);
+    var bids = JSON.parse(localStorage.bids);
+    bid.name = '竞价' + (bids.length + 1);
+    bids.push(bid);
+    localStorage.bids = JSON.stringify(bids);
+}
