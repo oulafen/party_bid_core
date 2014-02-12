3cab512ca95e07417a6a7274b92fabf93e2b6d15
xiaofen <oulafen@gmail.com>
2014-02-12 14:23:47 +0800

    data_structure_3-->完成render_sign_ups  功能

--- data_structure_3/src/SMS.js
+++ data_structure_3/src/SMS.js
@@ -1,3 +1,40 @@
-/**
- * Created by xiaofen on 14-2-12.
- */
+var native_accessor = {
+
+    send_sms: function (phone, message) {
+        native_access.send_sms({"receivers": [
+            {"name": 'name', "phone": phone}
+        ]}, {"message_content": message});
+    },
+
+    receive_message: function (sms_json) {
+        if (typeof this.process_received_message === 'function') {
+            this.process_received_message(sms_json);
+        }
+    },
+
+    process_received_message: function (sms_json) {
+        if (!check_message(sms_json)) {
+            return;
+        }
+        native_accessor[get_bm_or_jj(sms_json)](sms_json);
+    },
+
+    'BM': function (sms_json) {
+        SMSSignUp.check_sign_up_activity(SMSSignUp.reconstruct_sign_up_message(sms_json));
+    },
+    'JJ': function (sms_json) {
+        Bidding.check_bid_activity(Bidding.reconstruct_bid_message(sms_json));
+    }
+};
+function check_message(message_json) {
+    var message_flag = message_json.messages[0].message.substring(0, 2);
+    if ((message_flag == 'JJ' || message_flag == 'BM') && message_json.messages[0].message.length > 2) {
+        return true;
+    }
+}
+function get_bm_or_jj(message_json) {
+    return message_json.messages[0].message.substring(0, 2).toUpperCase();
+}
+function notify_sms_received(message_json) {
+    native_accessor.receive_message(message_json);
+}
--- data_structure_3/src/SignUp.js
+++ data_structure_3/src/SignUp.js
@@ -1,3 +1,14 @@
 /**
  * Created by xiaofen on 14-2-12.
  */
+function SignUp(name, phone, activity_id) {
+    this.name = name;
+    this.phone = phone;
+    this.activity_id = activity_id
+}
+SignUp.render_sign_ups = function (activity_id) {
+    var sign_ups = JSON.parse(localStorage.sign_ups);
+    return _.filter(sign_ups, function (sign_up) {
+        return sign_up.activity_id == activity_id;
+    });
+}
